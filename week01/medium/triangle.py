cc83a3c9599347a46ec5193100db2816dd480b33
Joseph Stocks <jpstocks63@gmail.com>
2020-11-11 21:03:45 -0600

    simplified triangle.py

--- week01/medium/triangle.py
+++ week01/medium/triangle.py
@@ -2,26 +2,21 @@
 # subtract_amount = 0
 # complete_string = ""
 # for row in range(height):
+#     temp_string = ""
 #     for column in range(2 * height - 1):
 #         if column >= subtract_amount and column <= 2 * height - 2 - subtract_amount:
-#             print("*", end="")
+#             temp_string += "*"
 #         else:
-#             print("X", end="")
-#     print()
+#             temp_string += " "
+#     temp_string += "\n"
 #     subtract_amount += 1
+#     complete_string = temp_string + complete_string
 
+# print(complete_string)
+##############################################
+# ANOTHER VERSION
 height = int(input("Triangle height? "))
-subtract_amount = 0
-complete_string = ""
-for row in range(height):
-    temp_string = ""
-    for column in range(2 * height - 1):
-        if column >= subtract_amount and column <= 2 * height - 2 - subtract_amount:
-            temp_string += "*"
-        else:
-            temp_string += " "
-    temp_string += "\n"
-    subtract_amount += 1
-    complete_string = temp_string + complete_string
-
-print(complete_string)
+stars = 1
+for blanks in range(5, 0, -1):
+    print(" " * blanks + "*" * stars)
+    stars += 2
